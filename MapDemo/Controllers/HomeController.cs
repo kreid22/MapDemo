535765e5a8bbc474850177fe3d1cadcd121f78fb
Kelsey Reid <kelsey.reid@tkxs.com>
2024-12-05 14:00:21 -0600

    initial map poc

---
+++ MapDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MapDemo.Models;
+using System.IO;
+
+namespace MapDemo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Map()
+    {
+        return Redirect("/45CanadaGrowersMap/map1.html");
+    }    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
